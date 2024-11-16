032fbe3236c4ebd5cc94ca35ff26521bcf080d3b
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-11-16 23:31:39 +0530

    code_packaged

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ import os
 from pathlib import Path
 from setuptools import find_packages, setup
 
-# Metadata of package
 NAME = 'sentiment_prediction'
-DESCRIPTION = 'An end-to-end machine learning project for Twitter sentiment classification'
-URL = 'https://github.com/vijaytakbhate2002/Twitter-Post-Sentiment-Prediction--NLP-.git'
+DESCRIPTION = 'Sentiment classification of twitter post in four classes [negative, positive, nutral, irrelevant]'
+URL = 'https://github.com/vijaytakbhate2002/sentiment_prediction_python_package.git'
 EMAIL = 'vijaytakbhate20@gmail.com'
 AUTHOR = 'Vijay Dipak Takbhate'
-REQUIRES_PYTHON = '>=3.7'
+REQUIRES_PYTHON = '>=3.12'
 
 pwd = os.path.abspath(os.path.dirname(__file__))
 
-# Get the list of packages to be installed
 def list_reqs(fname='requirements.txt'):
     try:
         with io.open(os.path.join(pwd, fname), encoding='utf-16') as f:
@@ -27,7 +25,6 @@ try:
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
 ROOT_DIR = Path(__file__).resolve().parent
 PACKAGE_DIR = ROOT_DIR / NAME
 
@@ -51,7 +48,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
